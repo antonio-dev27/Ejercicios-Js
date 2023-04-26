bf6295a2a8ae8da44956f16058995ea8b8d1f3ba
Antonio_dev <130109405+antonio-dev27@users.noreply.github.com>
2023-04-25 21:08:34 -0500

    ejercicio3

---
+++ ejercicio-3.js
@@ -0,0 +1,24 @@
+/*Constantes.
+Son datos que no cambian en la ejecución del código, se definen de la siguiente manera.
+    const NOMBRE_CONSTANTE = valorConstante
+Por ejemplo, en mátematicas el valor de PI no cambia, es cosntante. 
+Vea el siguiente código*/
+
+const PI=3.1416
+//Otra manera de concatenar texto es: `Texto ${variable / constante}`
+console.log(`El valor de PI es: ${PI}`)
+
+const   segundosEnMinuto = 60
+const   minutosEnHora = 60
+const   horasEnDia = 24 
+
+console.log(`Un minuto tiene ${segundosEnMinuto} segundos, hora tiene ${minutosEnHora} minutos, y un dia tiene ${horasEnDia} horas`)    
+
+
+
+
+// "Sintaxis vieja"
+const userInfo = 'User info: ' + name + ' ' + surname + ' ' + telephone;
+
+// "Nueva sintaxis"
+const userInfo = `User info: ${name} ${surname} ${telephone}`;
