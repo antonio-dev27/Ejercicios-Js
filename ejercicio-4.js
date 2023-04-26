5c73d6a4cdeef7563ee6ab9a7919dfe5492451b9
Antonio_dev <130109405+antonio-dev27@users.noreply.github.com>
2023-04-25 21:43:20 -0500

    ejercicio4

--- ejercicio-4.js
+++ ejercicio-4.js
@@ -8,29 +8,41 @@ Vea las siguientes instrucciones de código.*/
 /*Para definir una cadena de texto se puede hacer con comillas dobles o sencillas de la siguiente manera:
     -let / var / const = "cadena de texto"
     -let / var / const = 'cadena de texto'*/
-    let cadenaTexto = "Hola mundo!"
-    console.log(cadenaTexto)
-    console.log(typeof(cadenaTexto))
+    //let cadenaTexto = "Hola mundo!"
+    //console.log(cadenaTexto)
+    //console.log(typeof(cadenaTexto))
     
     //Las cadenas de texto se pueden concatenar en una variable con un +, de la siguiente manera.
     
-    let nombre = "Jaime"
-    cadenaTexto= "Hola!" + " Mi nombre es " + nombre
-    console.log(cadenaTexto)
+    //var nombre = "Jaime"
+    //cadenaTexto= "Hola!" + " Mi nombre es " + nombre
+    //console.log(cadenaTexto)
     
     /*Para definir un tipo número no se agregan comillas dobles ni sencillas. Como se ve a continuación*/
     
-    let edad=35
-    console.log(`${nombre} tiene ${edad} años`)
-    console.log(typeof(edad))
+    //let edad=35
+    //console.log(`${nombre} tiene ${edad} años`)
+    //console.log(typeof(edad))
     
     //Para definir booleanos se utilizan las palabras reservadas true o false según sea el caso.
     
-    let verdadero = true
-    console.log("El veradero se define con",verdadero)
-    console.log(typeof(verdadero))
+    //let verdadero = true
+    //console.log("El veradero se define con",verdadero)
+    //console.log(typeof(verdadero))
     
     //Cuando no se asigna ningun valor es de tipo undefined
-    let color
-    console.log(`Mi color favorito es ${color}`)
-    console.log(typeof(color))
+    //let color
+    //console.log(`Mi color favorito es ${color}`)
+    //console.log(typeof(color))
+
+    //CODIGO PARA ENTENDER LOS TIPOS DE VARIABLES MAS USADOS
+
+    let nombre = "Jaime";
+    let apellido ="Antonio";
+    let edad = 35;
+    let cancion = "snuff"
+    let respuesta = true
+
+    console.log(`Hola, mi nombre es ${nombre} ${apellido} y tengo ${edad} años, mi cancion favorita es ${cancion} y la respuesta es ${respuesta}`)
+
+
